ba0e1ab0b4bf2f87e0f79ed3688d1baef10ad05d
Jordan Collier <j.collier@uws.edu.au>
2018-10-09 14:58:19 +0200

    Added function to overwrite config
    Added overwrite_config(), which takes a config file and overwrites it, appending a new section and its dictionary elements.

--- config_parser.py
+++ config_parser.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 
 import argparse
-
+import ConfigParser
+import ast
 
 def parse_args():
     """
@@ -22,9 +23,6 @@ def parse_config(filename):
     should represent task parameters and values respectively.
     """
 
-    import ConfigParser
-    import ast
-
     config = ConfigParser.SafeConfigParser()
     config.read(filename)
 
@@ -41,11 +39,27 @@ def parse_config(filename):
             taskvals[section][option] =\
                             ast.literal_eval(config.get(section, option))
 
-    print(taskvals)
-    input()
-    return taskvals
+    return taskvals,config
+
+def overwrite_config(filename,additional_dict={},additional_sec=''):
+
+    config_dict,config = parse_config(filename)
+
+    if additional_sec not in config.sections():
+        config.add_section(additional_sec)
+
+        for key in additional_dict.keys():
+            config.set(additional_sec, key, str(additional_dict[key]))
+
+            config_file = open(filename, 'w')
+            config.write(config_file)
+
+    else:
+        print '{0} section exists in {1}. Will not overwrite.'.format(additional_sec,filename)
+
 
 if __name__ == '__main__':
     cliargs = parse_args()
-    taskvals = parse_config(cliargs.config)
-    print(taskvals)
+    taskvals,config = parse_config(cliargs.config)
+    print taskvals
+
