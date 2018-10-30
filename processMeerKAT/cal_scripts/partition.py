59152b646c5b27b062572e9b9409f981fb44de7b
Srikrishna Sekhar <sekhars@students.iiserpune.ac.in>
2018-10-30 11:55:15 +0530

    Added check_refant to partition.py
    If `calcrefant=False` in the input config file, this checks whether the
    specified reference antenna exists in the MS, and complains and quits if
    it doesn't.
    
    This check is already done during the build stage of the config file,
    which emits a warning and gives the user a chance to change the
    reference antenna. The check in `partition.py` quits if the specified
    refant doesn't exist, since all of the calibration routines will fail if
    the antenna doesn't exist.

--- processMeerKAT/cal_scripts/partition.py
+++ processMeerKAT/cal_scripts/partition.py
@@ -8,6 +8,7 @@ import os
 #from processMeerKAT import config_parser
 import config_parser
 from config_parser import validate_args as va
+from cal_scripts import get_fields
 
 # Get the name of the config file
 args = config_parser.parse_args()
@@ -16,7 +17,14 @@ print(args)
 # Parse config file
 taskvals, config = config_parser.parse_config(args['config'])
 
-# Partition
 visname = va(taskvals, 'data', 'vis', str)
+calcrefant = va(taskvals, 'crosscal', 'calcrefant', bool, default=False)
+refant = va(taskvals, 'crosscal', 'refant', str, default='m005')
+
+# Check if the reference antenna exists, and complain and quit if it doesn't
+if not calcrefant:
+    get_fields.check_refant(MS=visname, refant=refant, warn=False)
+
+# Run partition
 mvis = os.path.split(visname.replace('.ms', '.mms'))[1]
 partition(vis=visname, outputvis=mvis, createmms=True, datacolumn='DATA')
