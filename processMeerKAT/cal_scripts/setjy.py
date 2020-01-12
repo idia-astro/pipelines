Bugfix: setjy.py incorrect flux scaling
The modifications to add full Stokes models of either 3C138 or 3C286 in
setjy had a bug where it would set the full Stokes model for _all_ the
fields in the MS. This is now fixed.

Also a small bugfix for calc_refant.py to account for antennas that
exist in the metadata but have no correlations.

processMeerKAT/cal_scripts/setjy.py
@@ -14,7 +14,7 @@ import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(format="%(asctime)-15s %(levelname)s: %(message)s", level=logging.INFO)
 
-def get_y_value_from_linear_fit(xInput, xDataList, yDataList):
+def linfit(xInput, xDataList, yDataList):
     """
 
     """
@@ -56,14 +56,18 @@ def do_setjy(visname, spw, fields, standard):
 
 
     # Check if 3C286 exists in the data
-    is3C286 = True
+    is3C286 = False
     try:
-        calibrator_3C286 = list(set(["3C286", "1328+307", "1331+305", "J1331+3030"]).intersection(set(fieldnames)))
+        calibrator_3C286 = list(set(["3C286", "1328+307", "1331+305", "J1331+3030"]).intersection(set(fieldnames)))[0]
     except IndexError:
-        is3C286 = False
+        calibrator_3C286 = []
+
+    if len(calibrator_3C286):
+        is3C286 = True
+        id3C286 = str(msmd.fieldsforname(calibrator_3C286)[0])
 
     if is3C286:
-        logger.info("Detected calibrator name(s):  %s", ", ".join(calibrator_3C286))
+        logger.info("Detected calibrator name(s):  %s" % calibrator_3C286)
         logger.info("Flux and spectral index taken/calculated from:  https://science.nrao.edu/facilities/vla/docs/manuals/oss/performance/fdscale")
         logger.info("Estimating polarization index and position angle of polarized emission from linear fit based on: Perley & Butler 2013 (https://ui.adsabs.harvard.edu/abs/2013ApJS..204...19P/abstract)")
         # central freq of spw
@@ -71,17 +75,17 @@ def do_setjy(visname, spw, fields, standard):
         freqList = np.array([1.05, 1.45, 1.64, 1.95]) * 1e9
         # fractional linear polarisation
         fracPolList = [0.086, 0.095, 0.099, 0.101]
-        polindex = get_y_value_from_linear_fit(spwMeanFreq, freqList, fracPolList)
-        logger.info("Predicted polindex at frequecny %s: %s", spwMeanFreq, polindex)
+        polindex = linfit(spwMeanFreq, freqList, fracPolList)
+        logger.info("Predicted polindex at frequency %s: %s", spwMeanFreq, polindex)
         # position angle of polarized intensity
         polPositionAngleList = [33, 33, 33, 33]
-        polangle = get_y_value_from_linear_fit(spwMeanFreq, freqList, polPositionAngleList)
-        logger.info("Predicted pol angle at frequecny %s: %s", spwMeanFreq, polangle)
+        polangle = linfit(spwMeanFreq, freqList, polPositionAngleList)
+        logger.info("Predicted pol angle at frequency %s: %s", spwMeanFreq, polangle)
 
         reffreq = "1.45GHz"
         logger.info("Ref freq %s", reffreq)
         setjy(vis=visname,
-            field=setjyname,
+            field=id3C286,
             scalebychan=True,
             standard="manual",
             fluxdensity=[14.6, 0.0, 0.0, 0.0],
@@ -93,14 +97,18 @@ def do_setjy(visname, spw, fields, standard):
 
 
     # Check if 3C138 exists in the data
-    is3C138 = True
+    is3C138 = False
     try:
-        calibrator_3C138 = set(["3C138", "0518+165", "0521+166", "J0521+1638"]).intersection(set(fieldnames))
+        calibrator_3C138 = list(set(["3C138", "0518+165", "0521+166", "J0521+1638"]).intersection(set(fieldnames)))[0]
     except IndexError:
-        is3C138 = False
+        calibrator_3C138 = []
+
+    if len(calibrator_3C138):
+        is3C138 = True
+        id3C138 = str(msmd.fieldsforname(calibrator_3C138)[0])
 
     if is3C138:
-        logger.info("Detected calibrator name(s):  %s", ", ".join(calibrator_3C138))
+        logger.info("Detected calibrator name(s):  %s" % calibrator_3C138)
         logger.info("Flux and spectral index taken/calculated from:  https://science.nrao.edu/facilities/vla/docs/manuals/oss/performance/fdscale")
         logger.info("Estimating polarization index and position angle of polarized emission from linear fit based on: Perley & Butler 2013 (https://ui.adsabs.harvard.edu/abs/2013ApJS..204...19P/abstract)")
         # central freq of spw
@@ -108,19 +116,17 @@ def do_setjy(visname, spw, fields, standard):
         freqList = np.array([1.05, 1.45, 1.64, 1.95]) * 1e9
         # fractional linear polarisation
         fracPolList = [0.056, 0.075, 0.084, 0.09]
-        polindex = get_y_value_from_linear_fit(spwMeanFreq, freqList, fracPolList)
-        logger.info("Predicted polindex at frequecny %s: %s", spwMeanFreq, polindex)
+        polindex = linfit(spwMeanFreq, freqList, fracPolList)
+        logger.info("Predicted polindex at frequency %s: %s", spwMeanFreq, polindex)
         # position angle of polarized intensity
         polPositionAngleList = [-14, -11, -10, -10]
-        polangle = get_y_value_from_linear_fit(
-            spwMeanFreq, freqList, polPositionAngleList
-        )
-        logger.info("Predicted pol angle at frequecny %s: %s", spwMeanFreq, polangle)
+        polangle = linfit(spwMeanFreq, freqList, polPositionAngleList)
+        logger.info("Predicted pol angle at frequency %s: %s", spwMeanFreq, polangle)
 
         reffreq = "1.45GHz"
         logger.info("Ref freq %s", reffreq)
         setjy(vis=visname,
-            field=setjyname,
+            field=id3C138,
             scalebychan=True,
             standard="manual",
             fluxdensity=[8.26, 0.0, 0.0, 0.0],