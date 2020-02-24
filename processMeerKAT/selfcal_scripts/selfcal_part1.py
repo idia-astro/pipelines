427bacab6a43b9906f6dde2697db88326c241b54
Srikrishna Sekhar <srikrishna@idia.ac.za>
2020-02-24 13:28:20 +0200

    Implemented selfcal_part1 and selfcal_part2
    Now just need to debug...

--- processMeerKAT/selfcal_scripts/selfcal_part1.py
+++ processMeerKAT/selfcal_scripts/selfcal_part1.py
@@ -7,6 +7,38 @@ import os
 import config_parser
 from config_parser import validate_args as va
 
+def selfcal_part1(vis, imagename, imsize, cell, gridder, wprojplanes,
+                   deconvolver, robust, niter, multiscale, threshold,
+                   nterms, regionfile, restart_no, nloop, solint, calmode,
+                   atrous, loop):
+
+    ll = params['loop']
+
+    if ll == 0:
+        imagename = vis.replace('.ms', '') + '_im_%d_0' % (ll + restart_no)
+        regionfile = ''
+    else:
+        imagename = vis.replace('.ms', '') + '_im_%d' % (ll + restart_no):
+
+        if ll < params['nloop']:
+            caltable = vis.replace('.ms', '') + '.gcal%d' % (ll + restart_no)
+
+            applycal(vis=vis, selectdata=False, gaintable=caltable, parang=True)
+
+            flagdata(vis=vis, mode='rflag', datacolumn='RESIDUAL', field='', timecutoff=5.0,
+                    freqcutoff=5.0, timefit='line', freqfit='line', flagdimension='freqtime',
+                    extendflags=False, timedevscale=3.0, freqdevscale=3.0, spectralmax=500,
+                    extendpols=False, growaround=False, flagneartime=False, flagnearfreq=False,
+
+
+    tclean(vis=vis, selectdata=False, datacolumn='corrected', imagename=imagename,
+            imsize=imsize, cell=cell, stokes='I', gridder=gridder,
+            wprojplanes = wprojplanes, deconvolver = deconvolver, restoration=True,
+            weighting='briggs', robust = robust, niter=niter[ll], multiscale=multiscale[ll],
+            threshold=threshold[ll], nterms=nterms[ll],
+            savemodel='none', pblimit=-1, mask=regionfile, parallel = True)
+
+
 if __name__ == '__main__':
     # Get the name of the config file
     args = config_parser.parse_args()
@@ -14,6 +46,12 @@ if __name__ == '__main__':
     # Parse config file
     taskvals, config = config_parser.parse_config(args['config'])
     params = taskvals['selfcal']
+
     for arg in ['multiscale','nterms','calmode','atrous']:
         params[arg] = [params[arg]] * len(params['niter'])
-    selfcal(**params)
+
+    if 'loop' not in params:
+        params['loop'] = 0
+
+    selfcal_part1(**params)
+
--- processMeerKAT/selfcal_scripts/selfcal_part2.py
+++ processMeerKAT/selfcal_scripts/selfcal_part2.py
@@ -2,10 +2,80 @@
 #See processMeerKAT.py for license details.
 
 import sys
+import glob
+import shutil
 import os
 
 import config_parser
 from config_parser import validate_args as va
+
+def predict_model(vis, imagename, imsize, cell, gridder, wprojplanes,
+                      deconvolver, robust, niter, multiscale, threshold, nterms,
+                      regionfile):
+
+    # Rename image products for predict
+    for im in glob.glob(imagename + ".*"):
+        ext = im.replace(imagename, '')
+        #name, ext = os.path.splitext(im)
+        out = 'tmpim.im' + ext
+        shutil.copytree(im, out)
+
+    # Remove image and model image
+    shutil.rmtree(glob.glob('tmpim.im.image*'))
+    shutil.rmtree(glob.glob('tmpim.im.model*'))
+
+    startmodel = glob.glob(imagename + '.model*')
+
+    tclean(vis=vis, selectdata=False, datacolumn='corrected', imagename='tmpim.im',
+            imsize=imsize, cell=cell, stokes='I', gridder=gridder,
+            wprojplanes = wprojplanes, deconvolver = deconvolver, restoration=False,
+            weighting='briggs', robust = robust, niter=0, multiscale=multiscale[ll],
+            threshold=threshold[ll], nterms=nterms[ll], calcpsf=False, calcres=False,
+            startmodel = startmodel, savemodel='modelcolumn', pblimit=-1,
+            mask=regionfile, parallel = False)
+
+
+    shutil.rmtree("tmpim.im*")
+
+def selfcal_part2(vis, imagename, imsize, cell, gridder, wprojplanes,
+                   deconvolver, robust, niter, multiscale, threshold,
+                   nterms, regionfile, restart_no, nloop, solint, calmode,
+                   atrous, loop):
+
+    ll = params['loop']
+
+    # If it's the first round of selfcal and regionfile is blank, only run
+    # BDSF and quit.
+    if len(regionfile) == 0 and ll == 0:
+        do_gaincal = False
+    else:
+        do_gaincal = True
+
+    if atrous[ll]:
+        atrous_str = '--atrous-do'
+    else:
+        atrous_str = ''
+
+    if nterms[ll] > 1:
+        bdsmname = imagename + ".image.tt0"
+    else:
+        bdsmname = imagename + ".image"
+
+    regionfile = imagename + ".casabox"
+    os.system('/usr/bin/python bdsm_model.py {} {} --thresh-isl 20 '
+        '--thresh-pix 10 {} --clobber --adaptive-rms-box '
+        '--rms-map'.format(bdsmname, regionfile, atrous_str))
+
+    if do_gaincal:
+        predict_model(vis, imagename, imsize, cell, gridder, wprojplanes,
+                      deconvolver, robust, niter, multiscale, threshold, nterms,
+                      regionfile)
+
+        gaincal(vis=vis, caltable=caltable, selectdata=False, solint=solint[ll],
+                calmode=calmode[ll], append=False, parang=True)
+
+    return do_gaincal
+
 
 if __name__ == '__main__':
     # Get the name of the config file
@@ -14,6 +84,11 @@ if __name__ == '__main__':
     # Parse config file
     taskvals, config = config_parser.parse_config(args['config'])
     params = taskvals['selfcal']
+
     for arg in ['multiscale','nterms','calmode','atrous']:
         params[arg] = [params[arg]] * len(params['niter'])
-    selfcal(**params)
+
+    ran_gaincal = selfcal_part2(**params)
+
+    if ran_gaincal:
+        config_parser.overwrite_config(args['config'], conf_dict={'loop' : params['loop']+1})
