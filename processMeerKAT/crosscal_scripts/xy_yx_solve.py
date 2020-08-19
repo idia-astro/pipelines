140853f3514a4ad6e70a8a3956f3f3cba10d5066
Jordan Collier <j.collier@uws.edu.au>
2020-08-19 09:09:39 +0200

    Bugfix

--- processMeerKAT/crosscal_scripts/xy_yx_solve.py
+++ processMeerKAT/crosscal_scripts/xy_yx_solve.py
@@ -25,7 +25,7 @@ def polfield_name(visname):
     polfield = ''
     if any([ff in ["3C286", "1328+307", "1331+305", "J1331+3030"] for ff in fieldnames]):
         polfield = '3C286'
-    elif any(ff in ["3C138", "0518+165", "0521+166", "J0521+1638"] for ff in fieldnames]):
+    elif any([ff in ["3C138", "0518+165", "0521+166", "J0521+1638"] for ff in fieldnames]):
         polfield = '3C138'
     else:
         logger.warning("No valid polarization field found. Defaulting to use the phase calibrator to solve for XY phase.")
