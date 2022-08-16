c8bf96c6e1c1f2dd5fa1e3b59fe24b4d904a83d8
jordatious <j.collier@uws.edu.au>
2022-08-17 01:45:49 +0200

    Update setup.sh to add warning about removing branch.

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ export PATH=$PATH:$dir
 export PYTHONPATH=$PYTHONPATH:$dir
 export SINGULARITYENV_PYTHONPATH="$PYTHONPATH:\$PYTHONPATH"
 git config --global --add safe.directory $(dirname $BASH_SOURCE)
+echo This branch of the pipeline \(sourced from $dir\) will be removed on 30 August 2022. To use the latest version of the pipeline \(v2.0\), please source the master branch.
